40896af3faf51598969acf13683bad6892248456
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-07-29 18:05:31 +0000

    first push: Readme.md and push.sh created

---
+++ push.sh
@@ -0,0 +1,6 @@
+git config --global user.email "seydiahmedelcheikh@gmail.com"
+git config --global user.name "mouhameddiouf"
+git config credential.helper store
+git add .
+git commit -m "first push: Readme.md and push.sh created"
+git push
